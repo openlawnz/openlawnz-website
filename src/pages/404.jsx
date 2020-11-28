b428ce852876c3f62c1bc65fc817b6878e2e465d
Ben Rauzi <ben.rauzi@outlook.com>
2020-11-28 22:45:40 +1300

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -19,8 +19,7 @@ const NotFoundPage = ({ data }) => {
                     If you believe this is an error, contact our <a href="mailto:enquiries@openlaw.nz">development team.</a>
                   </p>
 
-                  <form className="contact-form" name="contact" method="POST" netlify-honeypot="bot-field" netlify data-netlify="true">
-                    <input type="hidden" name="bot-field" />
+                  <form className="contact-form" name="contact" method="POST" netlify data-netlify="true">
                     <input type="hidden" name="form-name" value="contact" />
 
                     <label htmlFor="name" className="show-for-sr">Your Name: </label>    
