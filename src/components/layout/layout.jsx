9ec0a640e47d4fd0f9b1bd8d14c7acadc58586ae
Ben Rauzi <ben.rauzi@outlook.com>
2021-09-11 03:19:57 +1200

    Fix build error from accidental revert

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Header from "../Header/Header"
-import Footer from "../Footer/Footer"
+import Header from "../header/header"
+import Footer from "../footer/footer"
 
 import "@/css/App.css"
 
