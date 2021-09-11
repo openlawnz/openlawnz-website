c7842bce03628c8ec4c1797ebba1a3d266eb9423
Ben Rauzi <ben.rauzi@outlook.com>
2021-09-11 14:08:55 +1200

    Add Google Analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-147450154-1`,
       },
     },
     {
