d937cfea2cd033aaf43dd1f79154afb1618091eb
Ben Rauzi <ben.rauzi@outlook.com>
2021-09-11 15:31:18 +1200

    Add Window Check in 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,41 +3,52 @@ import Layout from "@/components/layout/layout"
 import HeroSmall from "@/components/hero/hero-small"
 import SEO from "@/components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-      <SEO
-        title="404 Page not Found"
-        description="Page not found"
-      />
-      <HeroSmall title="Page not found"/>     
-      <div className="inner">
-        <div className="body-wrap content-page right-on-top">
-          <div className="body-left">
-            <div id="mission-statement" className="content-section">
-              <h2 className="body-title">Page Not Found</h2>
-                <p>
-                  If you believe this is an error, contact our <a href="mailto:enquiries@openlaw.nz">development team.</a>
-                </p>
+const NotFoundPage = () => {
+  const browser = typeof window !== "undefined" && window
 
-                <form className="contact-form" name="contact" method="POST" netlify data-netlify="true">
-                  <input type="hidden" name="form-name" value="contact" />
+  return (
+    <Layout>
+      {
+        browser &&
+        <>
+          <SEO
+            title="404 Page not Found"
+            description="Page not found"
+          />
+          <HeroSmall title="Page not found"/>     
+          <div className="inner">
+            <div className="body-wrap content-page right-on-top">
+              <div className="body-left">
+                <div id="mission-statement" className="content-section">
+                  <h2 className="body-title">Page Not Found</h2>
+                    <p>
+                      If you believe this is an error, contact our <a href="mailto:enquiries@openlaw.nz">development team.</a>
+                    </p>
 
-                  <label htmlFor="name" className="show-for-sr">Your Name: </label>    
-                  <input placeholder="Name" type="text" name="name" />
+                    <form className="contact-form" name="contact" method="POST" netlify data-netlify="true">
+                      <input type="hidden" name="form-name" value="contact" />
 
-                  <label htmlFor="email" className="show-for-sr">Your Email: </label>
-                  <input placeholder="Email" type="email" name="email" />
+                      <label htmlFor="name" className="show-for-sr">Your Name: </label>    
+                      <input placeholder="Name" type="text" name="name" />
 
-                  <label htmlFor="message" className="show-for-sr">Message: </label> 
-                  <textarea placeholder="Message" name="message"></textarea>
+                      <label htmlFor="email" className="show-for-sr">Your Email: </label>
+                      <input placeholder="Email" type="email" name="email" />
 
-                  <button type="submit">Send</button>
-                </form>
+                      <label htmlFor="message" className="show-for-sr">Message: </label> 
+                      <textarea placeholder="Message" name="message"></textarea>
+
+                      <button type="submit">Send</button>
+                    </form>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-  </Layout>
-)
+        </>
+      }
+        
+    </Layout>
+  )
+  
+}
 
 export default NotFoundPage
